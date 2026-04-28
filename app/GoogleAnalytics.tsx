081535a721d0e1305702cd6aaaec05215ef1f94b
Basit Abbasi <abasitabbasi99@gmail.com>
2026-04-27 22:13:48 -0400

    Add Google Analytics tracking component

---
+++ app/GoogleAnalytics.tsx
@@ -0,0 +1,31 @@
+import Script from "next/script";
+
+const measurementId = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID || "G-JEMNO4ZWC8";
+
+export default function GoogleAnalytics() {
+  if (!measurementId) return null;
+
+  return (
+    <>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${measurementId}`}
+        strategy="afterInteractive"
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${measurementId}', {
+              page_path: window.location.pathname,
+              send_page_view: true
+            });
+          `
+        }}
+      />
+    </>
+  );
+}
