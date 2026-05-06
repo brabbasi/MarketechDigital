6bc3bceab1c3fe5f56858ece82725f9f9f82dcdb
Basit Abbasi <abasitabbasi99@gmail.com>
2026-05-06 14:14:33 -0400

    Serve larger favicon for direct favicon URL

--- app/favicon.ico/route.ts
+++ app/favicon.ico/route.ts
@@ -1,8 +1,8 @@
-import { FAVICON_48_PNG, faviconPngResponse } from "../favicon-assets";
+import { FAVICON_192_PNG, faviconPngResponse } from "../favicon-assets";
 
 export const dynamic = "force-static";
 export const revalidate = false;
 
 export function GET() {
-  return faviconPngResponse(FAVICON_48_PNG);
+  return faviconPngResponse(FAVICON_192_PNG);
 }
